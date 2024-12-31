ddddef107de91be56d08dbd9ad08161798dac3b5
saynadhikary <sayan711226@gmail.com>
2024-12-31 22:55:49 +0530

    add fabicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
-import { Button } from "@/components/ui/button";
-import { getProjects } from "@/sanity/sanity-utils";
+// import { Button } from "@/components/ui/button";
+// import { getProjects } from "@/sanity/sanity-utils";
 
 export default async function Home() {
 
-const projects = await getProjects();
+// const projects = await getProjects();
   return (
     <div>
-      {projects.map(project=>(
+      {/* {projects.map(project=>(
         <div key={project._id}>{project.name}
         <Button>My Button</Button>
         </div>
-      ))}
+      ))} */}
+      <h1 className="text-center text-3xl md:text-5xl lg:text-7xl font-bold text-orange-600 m-10">Build your Website with us</h1>
+
     </div>
   );
 }
